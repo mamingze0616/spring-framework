7402c219eebb548c7fa7d1a8e495cae7abf1c985
mamingze <1132990723@qq.com>
2026-08-29 16:46:07 +0800

    feat(aop): 添加AOP功能实现和相关配置
    - 集成aspectjweaver依赖支持AOP功能
    - 创建Calculate接口定义基本计算方法
    - 实现TulingCalculate类提供具体计算逻辑
    - 配置EalyAopMainConfig提供多种AOP代理创建方式
    - 添加TulingLogAspect实现切面编程功能
    - 创建TulingLogAdvice和TulingLogInterceptor实现通知功能
    - 添加TulingMainClass和EalyAopDemo演示AOP使用
    - 实现引入功能支持ProgramCalculate接口
    - 提供MethodBeforeAdviceInterceptor实现前置通知拦截

---
+++ spring-tl-beans/src/main/java/com/tuling/TulingMainClass.java
@@ -0,0 +1,27 @@
+package com.tuling;
+
+import com.tuling.Introductions.ProgramCalculate;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+/**
+ * Created by xsls on 2019/6/10.
+ */
+public class TulingMainClass {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfig.class);
+
+//		/**/
+//		Calculate calculate = (Calculate) ctx.getBean("calculate");
+//		int retVal = calculate.div(2, 4);
+
+
+		ProgramCalculate pcalculate = (ProgramCalculate) ctx.getBean("calculate");
+		System.out.println(pcalculate.toBinary(100));
+
+
+	}
+
+}
